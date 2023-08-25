abd04c82cd57bdf4e7f27329461f789e92ba78e7
Marcus Oberdiek <moberdiek@gmx.de>
2023-08-25 11:52:41 +0200

    add Aktivity function

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,11 +1,20 @@
 import {useState} from "react";
+import { nanoid } from "nanoid";
 
 export default function ActivityForm() {
-const [activity, setActivity] = useState()
+const [activity, setActivity] = useState("")
 
     return (
 <>
         <h3>Add new Activity:</h3>
+<form>
+        onAddActivity={(event) => {
+          event.preventDefault();
+          setActivity([...activity, { name: inputValue, checked, id: nanoid(6) }]);
+          // Clears input after form submit.
+          setInputValue("");
+        }}
+
 
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" onClick={} />
@@ -15,5 +24,6 @@ const [activity, setActivity] = useState()
 
         <button type="submit">Submit</button>
  
-  </>  )
+  </form> 
+   </>)
 }
