4f81ce9a158eb0fbdd5bd1a5421e4475a10b0326
Marcus Oberdiek <moberdiek@gmx.de>
2023-08-28 12:13:50 +0200

    add filter and API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import ActivityForm from "./component/Form/Form.js";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 import List from "./component/List/List.js";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [weather, setWeather] = useState();
   const [activities, setActivities] = useLocalStorageState("activitiy", {
     defaultValue: [],
   });
@@ -13,15 +15,36 @@ function App() {
     setActivities([...activities, { id: uid(6), ...newActivity }]);
   }
 
-  const isGoodWeather = true;
+  //const isGoodWeather = false;
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(
+          "https://example-apis.vercel.app/api/weather/europe"
+        );
+        const data = await response.json();
+
+        setWeather(data);
+      } catch (error) {
+        console.error("Error fetching weather:", error);
+      }
+    };
+    const weatherInterval = setInterval(fetchWeather, 5000);
+
+    fetchWeather();
+    return () => clearInterval(weatherInterval);
+  }, []);
 
   const filteredActivities = activities.filter(
-    (activity) => activity.isForGoodWeather !== isGoodWeather
+    (activity) => activity.goodWeather === weather.isGoodWeather
   );
 
   return (
     <>
-      <List activities={filteredActivities} isGoodWeather={isGoodWeather} />
+      <List
+        activities={filteredActivities}
+        isGoodWeather={weather.isGoodWeather}
+      />
       <hr />
       <ActivityForm onAddActivity={handleAddActivity} />
     </>
