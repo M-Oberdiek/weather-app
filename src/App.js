51997002247d12431a74ba7bea84b473a8b877c0
Pia Lüling <p.lueling@gmail.com>
2023-08-28 09:46:34 +0200

    install uselocalstoragestate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import ActivityForm from "./component/Form/Form.js";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
-import List from "./component/LIST/List.js";
+import List from "./component/List/List.js";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
